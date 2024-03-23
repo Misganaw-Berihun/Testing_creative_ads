74f5074408971848f803726fcb9b0c88211a7c03
Misganaw-Berihun <misganawbmb@gmail.com>
2024-03-23 09:01:25 +0300

    extract events from ad-unit

---
+++ backend/scripts/event_extractor.py
@@ -0,0 +1,22 @@
+import re
+import requests
+
+def extract_events(url):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        js_content = response.text
+
+        send_event_pattern = r'sendEvent\("([^"]+)"'
+
+        send_events = re.findall(send_event_pattern, js_content)
+
+        print("First parameters from texts that start with 'SendEvent':")
+        events = []
+        for event in send_events:
+            events.append(event)
+        
+        return events
+
+    else:
+        print(f"Failed to fetch JavaScript file from {url}. Status code: {response.status_code}")
