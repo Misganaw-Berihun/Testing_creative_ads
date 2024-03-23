300a9823f08ae89966824441bedc860e16bb608c
Misganaw-Berihun <misganawbmb@gmail.com>
2024-03-23 08:58:00 +0300

    populate database

---
+++ backend/connection/populate_adunits.py
@@ -0,0 +1,26 @@
+import os
+import json
+from sqlalchemy.orm import sessionmaker
+
+from tables import Game, RedirectURLs, ThirdPartyEngagement, TTDMetadata, Base
+from add_adunit import add_ad_unit
+from postgres_connect import create_database_engine
+
+def populate_ad_units(session, ad_units_data):
+    for ad_unit in ad_units_data:
+        add_ad_unit(session, ad_unit)
+        
+    session.commit()
+
+if __name__ == "__main__":
+    engine = create_database_engine()
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    with open('json/ad_units.json') as ad_units_file:
+        ad_units_data = json.load(ad_units_file)
+        populate_ad_units(session, ad_units_data)
+
+    print('Ad units data successfully populated in the database.')
